4f9a753b985ecd307764939f900aa5ebe7c91b5a
uplRanabir <ranabir.mandal@utplco.com>
2024-03-26 16:49:10 +0530

    message send and get functionality done

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,16 +1,27 @@
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../store/useConversation";
+import { extractTime } from "../utils/extractTime";
 
-const Message = () => {
+const Message = ({message}) => {
+    const { authUser } = useAuthContext();
+    const { selectedConversation } = useConversation();
+    const fromMe = message.senderId === authUser._id;
+    const chatClassName = fromMe ? "chat-end" : "chat-start";
+    const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+    const myMsgBgColor = fromMe ? "bg-blue-500" : "";
+    const formattedTime = extractTime(message.createdAt);
+
   return (
-      <div className={`chat chat-end`}>
+      <div className={`chat ${chatClassName}`}>
           <div className='chat-image avatar'>
               <div className='w-10 rounded-full'>
                   <img 
                   alt='Tailwind CSS chat bubble component'
-                   src={"https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"} />
+                   src={profilePic} />
               </div>
           </div>
-          <div className={`chat-bubble text-white  pb-2`}>Hi</div>
-          <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>11:11</div>
+          <div className={`chat-bubble text-white  pb-2 ${myMsgBgColor}`}>{message.message}</div>
+          <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
       </div>
   )
 }
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
 import { IoSearchSharp } from "react-icons/io5";
+import useConversation from "../../store/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
 
 const SearchInput = () => {
+	const [searchInput, setSearchInput] = useState("");
+	const { setSelectedConversation } = useConversation();
+	const { conversations} = useGetConversations()
+
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if(!searchInput) return;
+		if (searchInput.length < 2){
+			return toast.error("Search input must be at least 2 characters")
+		}
+
+		const conversation = conversations.find((convrsn) => convrsn.fullName.toLowerCase().includes(searchInput.toLowerCase()));
+		if(conversation) {
+			setSelectedConversation(conversation);
+			setSearchInput("")
+		} else {
+			toast.error("No such user found")
+		}
+	};
 	return (
-		<form className='flex items-center gap-2'>
-			<input type='text' placeholder='Search…' className='input input-bordered rounded-full' />
+		<form className='flex items-center gap-2' onSubmit={handleSubmit}>
+			<input
+				type='text'
+				placeholder='Search…'
+				className='input input-bordered rounded-full'
+				value={searchInput}
+				onChange={(e) => setSearchInput(e.target.value)}
+			/>
 			<button type='submit' className='btn btn-circle bg-sky-500 text-white'>
 				<IoSearchSharp className='w-6 h-6 outline-none' />
 			</button>
