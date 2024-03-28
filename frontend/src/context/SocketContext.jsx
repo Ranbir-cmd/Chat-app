9f1cb16ab3d5903a685c39465ae3372e30e584fb
uplRanabir <ranabir.mandal@utplco.com>
2024-03-28 18:51:12 +0530

    deployed url added

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -62,7 +62,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:8000", {
+            const socket = io("https://letschat-jtbf.onrender.com/", {
                 query: {
                     userId: authUser._id,
                 },
